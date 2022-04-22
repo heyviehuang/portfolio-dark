a92cfd81320d811b3567748e092a4c5da0212afa
heyviehuang <heyviehuang@gmail.com>
2022-04-22 18:18:31 +0800

    -背景碎片RWD -新增動態導航功能

--- js/all.js
+++ js/all.js
@@ -1,4 +1,31 @@
 $(document).ready(function() {
+
+    $('.scrollTop').click(function(e) {
+        e.preventDefault();
+        var target = $(this).attr('href');
+        var targetPos = $(target).offset().top;
+        $('html,body').animate({
+            scrollTop: targetPos
+        }, 700);
+    });
+
+    $(window).scroll(function() {
+        var scrollPos = $(window).scrollTop();
+        var windowHeight = $(window).height();
+
+        $('.scrollTop').each(function() {
+            var target = $(this).attr('href');
+            var targetPos = $(target).offset().top;
+            var targetHeight = $(target).outerHeight();
+            if (targetPos - 1 <= scrollPos && (targetPos + targetHeight) > scrollPos) {
+                $('.scrollTop').removeClass('active');
+                $(this).addClass('active');
+            } else {
+                $(this).removeClass('active');
+            }
+        });
+    });
+
     $(".snow").let_it_snow({
         windPower: 0.01,
         speed: 0.001,
