e2c7c5d0773d16432254a17dc83bd1f9dcd20636
heyviehuang <heyviehuang@gmail.com>
2022-04-22 15:59:14 +0800

    -優化大頭照解析度 -新增建模作品內容 -隱藏相關連結 -RWD(除背景外)

--- js/all.js
+++ js/all.js
@@ -43,25 +43,6 @@ $(document).ready(function() {
     // }
     // $(window).scroll(showBtnCondition);
 
-    // $('.productList-item-btnAddCart').click(function(event) {
-    //     event.preventDefault();
-    //     alert('已將商品加入購物車!');
-    // });
-
-    // $('.menu-tab-productCategory li').click(function(event) {
-    //     event.preventDefault();
-    //     $(this).find('.tab-productCategory-bg').addClass('productCategory-item-active').parent().siblings().find('.tab-productCategory-bg').removeClass('productCategory-item-active');
-    // });
-
-    // $('.size-buttonList li').click(function(event) {
-    //     event.preventDefault();
-    //     $(this).find('button').toggleClass('size-item-active').parent().siblings().find('button').removeClass('size-item-active');
-    // });
-
-    // var mm = window.matchMedia("(max-width: 768px)");
-    // mm.addListener(resizeWidth);
-    // resizeWidth(mm);
-
     var swiper = new Swiper(".paintingWork-swiper", {
         spaceBetween: 30,
         centeredSlides: true,
@@ -78,6 +59,27 @@ $(document).ready(function() {
         'wrapAround': true
     })
 
+    var mm = window.matchMedia("(max-width: 415px)");
+    mm.addListener(resizeWidth);
+    resizeWidth(mm);
+
+    function resizeWidth(pMatchMedia) {
+        if (pMatchMedia.matches) {
+            var swiper = new Swiper(".modelingWorkBy3dsMax-swiper", {
+                slidesPerView: 1,
+                spaceBetween: 8,
+                slidesPerGroup: 1,
+                loop: true,
+            });
+        } else {
+            var swiper = new Swiper(".modelingWorkBy3dsMax-swiper", {
+                slidesPerView: 3,
+                spaceBetween: 16,
+                freeMode: true,
+            });
+        }
+    }
+
     $(window).scroll(function() {
         var scrollPos = $(window).scrollTop();
         var windowHeight = $(window).height();
@@ -89,11 +91,5 @@ $(document).ready(function() {
                 $(this).addClass('fadeIn');
             }
         });
-        //bg scroll
-        // $('.img-bg-bubble').css('transform', 'translateY(' + -scrollPos / 2 + 'px)');
-        // $('.img-bg-bubble2').css('transform', 'translateY(' + -scrollPos / 5 + 'px)');
-        // $('.img-bg-orange, .img-bg-orange2, .img-bg-orange3, .img-bg-orange4, .img-bg-orange5, .img-bg-orange6, .img-bg-orange7').css('transform', 'translateY(' + -scrollPos / 16 + 'px)');
     });
-
-
 });
